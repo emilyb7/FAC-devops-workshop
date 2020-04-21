7ad9cb245df4464dd30f3828a2ce143c214c0234
Sohil Pandya <sohilpandya@me.com>
2020-04-21 21:54:12 +0100

    bad test

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const Express = require("express");
 
 const app = Express();
 
-app.get("/", (req, res) => res.send("Hello, world!"));
+app.get("/", (req, res) => res.send("Hello, sohil!"));
 
 module.exports = app;
