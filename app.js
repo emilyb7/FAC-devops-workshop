f486991e67a7bef608e0f1f4d66907c7d821b302
frannyfra <francescadelaurentis1987@gmail.com>
2020-04-22 16:05:53 +0100

    forcing test

--- app.js
+++ app.js
@@ -2,10 +2,6 @@ const Express = require("express");
 
 const app = Express();
 
-const emily = "hello Emily";
-const franny = "hello Emily";
-const hello = "hello";
-
-app.get("/", (req, res) => res.send("Hello, world!"));
+app.get("/", (req, res) => res.send("Hello, emily!"));
 
 module.exports = app;
